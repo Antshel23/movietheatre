87a609c4f9a88c1679ea432e137f68c51e50210e
Antshel23 <antsheldon23@icloud.com>
2024-10-18 10:21:52 +0100

    association gets

--- routes/shows.js
+++ routes/shows.js
@@ -2,14 +2,26 @@ const { Show, User} = require('../models/index') //import both models from index
 const express = require('express')
 const showsRouter = express.Router() // define usersRouter as an express router, ready to declare CRUD functions upon
 
+//get all
 showsRouter.get('/', async (req, res) => {
     const shows = await Show.findAll()
     res.json(shows)
 })
 
+//get by id
 showsRouter.get('/:id', async (req, res) => {
     const show1 = await Show.findByPk(req.params.id)
     res.json(show1)
+})
+
+
+//get shows with users
+showsRouter.get('/:id/users', async (req, res) => {
+    const showUsers = await Show.findByPk(req.params.id, {
+        include: User
+    })
+
+    res.json(showUsers)
 })
 
 module.exports = showsRouter
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,25 @@ const { Show, User} = require('../models/index') //import both models from index
 const express = require('express')
 const usersRouter = express.Router() // define usersRouter as an express router, ready to declare CRUD functions upon
 
+//get all
 usersRouter.get('/', async (req, res) => {
     const users = await User.findAll()
     res.json(users)
 })
 
+//get by id
 usersRouter.get('/:id', async (req, res) => {
     const user1 = await User.findByPk(req.params.id)
     res.json(user1)
+})
+
+
+// get users with shows
+usersRouter.get('/:id/shows', async (req, res) => {
+    const userShows = await User.findByPk(req.params.id, {
+        include: Show
+    })
+    res.json(userShows)
 })
 
 module.exports = usersRouter
